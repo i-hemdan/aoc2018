3b2cf33fe6900f34e7ab008385f84ac7898d1d43
Hemdan <ihemdan@QLI.local>
2018-12-03 16:27:51 -0700

    changes

--- day 2/day2.hs
+++ day 2/day2.hs
@@ -11,14 +11,12 @@ main =  do
             putStrLn $ show $ hasExactlyN "aabbbccc" 'a' 1 0
 
 
-hasExactlyN::String -> Char -> Int -> Int -> Bool
-hasExactlyN string char count accum = case string of
+hasExactlyOf::String -> Char -> Int -> Int -> Bool
+hasExactlyOf string char count accum = case string of
     []          ->  case accum of       accum | accum == count  -> True 
                                         _                       -> False
 
-    (x:rest)    ->  case x of           x | x == char   -> hasExactlyN rest char count (accum+1)
-                                        _               -> hasExactlyN rest char count accum
-
-
+    (x:rest)    ->  case x of           x | x == char   -> rest `hasExactlyOf` char count (accum+1)
+                                        _               -> rest `hasExactlyOf` char count accum
 
 part1 list twos threes = undefined
