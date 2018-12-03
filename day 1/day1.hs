3fbea483fd3b4b5f4d42b21b94070d03807154a6
Hemdan <ihemdan@QLI.local>
2018-12-03 15:01:50 -0700

    final change

--- day 1/day1.hs
+++ day 1/day1.hs
@@ -18,10 +18,10 @@ resultingFrequency l = foldl (+) 0 l
 
 firstDupeF::Int -> Map.Map Int Bool -> [Int] -> [Int] -> Int
 firstDupeF  frequency frequency_map working_list original_list =
-                case working_list of    [] -> firstDupeF frequency frequency_map original_list original_list
+                case working_list of    []          -> firstDupeF frequency frequency_map original_list original_list
                                         (_:rest)    -> let  f = frequency + head working_list
                                                             f_in_map = Map.lookup f frequency_map in 
                                                                 case f_in_map of
-                                                                    Just True   -> f
                                                                     Nothing     ->  let new_map = Map.insert f True frequency_map in 
                                                                                         firstDupeF f new_map rest original_list
+                                                                    Just _      ->  f
