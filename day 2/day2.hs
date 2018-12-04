d39927a3e065caa39e86e55bb64d691cb99b7b2e
i hemdan <>
2018-12-04 00:03:41 -0700

    almost done day 2

--- day 2/day2.hs
+++ day 2/day2.hs
@@ -8,15 +8,35 @@ import qualified Data.Map.Strict as Map
 main =  do  
         f <- readFile "input.txt"
         let ids = words f in
-            putStrLn $ show $ hasExactlyN "aabbbccc" 'a' 1 0
+            putStrLn $ show $ calcCheckSum $ splitTwosThrees $ useHasOfOnInput ids []
 
 
-hasExactlyOf::String -> Char -> Int -> Int -> Bool
-hasExactlyOf string char count accum = case string of
-    []          ->  case accum of       accum | accum == count  -> True 
-                                        _                       -> False
+hasOf :: String -> Map.Map Char Int -> [(Char, Int)]
+hasOf string m = case string of
+    [] -> Map.toList m
+    (x:xs) -> let new_map = Map.insertWith (+) x 1 m in hasOf xs new_map
 
-    (x:rest)    ->  case x of           x | x == char   -> rest `hasExactlyOf` char count (accum+1)
-                                        _               -> rest `hasExactlyOf` char count accum
+useHasOfOnInput:: [String] -> [(Char, Int)]  -> [(Char, Int)]
+useHasOfOnInput input out = case input of
+    [] -> out
+    (x:xs) -> let   tarr = hasOf x Map.empty 
+                    new_out = lsJoin tarr out
+                    in
+                        useHasOfOnInput xs new_out
 
-part1 list twos threes = undefined
+splitTwosThrees:: [(Char, Int)] -> ([(Char, Int)], [(Char, Int)])
+splitTwosThrees ls = let    ls1 = [el | el <- ls, let (_, x) = el in x == 2]
+                            ls2 = [el2 | el2 <- ls, let (_, x2) = el2 in x2 == 3]
+                            in
+                                (ls1, ls2)
+
+calcCheckSum tuple_list = let   (a, b) = tuple_list
+                                lena = length a
+                                lenb = length b
+                                in
+                                    lena * lenb
+
+lsJoin:: [a] -> [a] -> [a]
+lsJoin a b = case a of
+    [] -> b
+    (x:xs) -> lsJoin xs (x:b)      
