7120f2b7711841d40846f434a673817ab4dad708
Hemdan <ihemdan@QLI.local>
2018-12-04 16:27:12 -0700

    testing fix count

--- day 3/day3.hs
+++ day 3/day3.hs
@@ -4,11 +4,31 @@ module Main where
 import Data.List.Split
 
 
-main = putStrLn . show $ new_fabric 1000 1000
+main = do 
+    f <- readFile "input.txt"
+    let 
+        claims = f
+        claimList = inputToClaimList claims
+        fabric = new_fabric 1000 1000
+        fabric1 = populateFabricWithClaims claimList fabric
+        count = count2orMoreClaims 0 fabric1
+        in
+            putStrLn  "hello"
 
 
 --Joint fabric and claim stuff
---TODO: doit
+count2orMoreClaims a !(Fabric w h ls) =
+    case ls of
+        [] -> a
+        !(x:xs) | (length $ si_claim_ids x) >= 2 -> count2orMoreClaims (a + 1) (Fabric w h xs) 
+
+populateFabricWithClaims claims fabric@(Fabric w h !a) =
+    case claims of
+        [] -> fabric
+        !(hd:tl) -> 
+            case hd of
+                (Claim id (x, y) (wd, ht)) -> let (SquareInch clms) = getInch fabric x y in populateFabricWithClaims tl (setInch fabric x y (SquareInch (id:clms))) 
+
 --Joint fabric and claim stuff end
 
 --Claim stuff
@@ -19,12 +39,12 @@ data Claim = Claim {
     }deriving (Show)
 
 readClaim str = 
-    let getId ((Claim _ pos dim), str) = let a = splitOn "@" str in ((Claim (a!!0) pos dim), (a!!1))
+    let getId !((Claim _ pos dim), str) = let a = splitOn "@" str in ((Claim (a!!0) pos dim), (a!!1))
 
-        getPos ((Claim id _ dim), str) = 
+        getPos !((Claim id _ dim), str) = 
             let a = splitOn ":" str in let b = splitOn "," (a!!0)in ((Claim id ( (read (b!!0))+1 , (read(b!!1))+1 ) dim), (a!!1))
 
-        getDim ((Claim id pos _), str) = let a = splitOn "x" str in ((Claim id pos ((read $ a!!0), (read $ a!!1))), str)
+        getDim !((Claim id pos _), str) = let a = splitOn "x" str in ((Claim id pos ((read $ a!!0), (read $ a!!1))), str)
 
         readClaim' = getDim . getPos . getId
 
