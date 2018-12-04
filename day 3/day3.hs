d22aff64d2cf10f9915ff22b71acd837cc174643
Hemdan <ihemdan@QLI.local>
2018-12-04 12:05:29 -0700

    alignment

--- day 3/day3.hs
+++ day 3/day3.hs
@@ -16,9 +16,8 @@ readClaim str =
         getId ((Claim _ pos dim), str) = let a = splitOn "@" str in ((Claim (a!!0) pos dim), (a!!1))
 
         getPos ((Claim id _ dim), str) = 
-            let a = splitOn ":" str 
-                b = splitOn "," (a!!0)                           in ((Claim id ((read (b!!0)), (read(b!!1))) dim), (a!!1))
-                
+            let a = splitOn ":" str in let b = splitOn "," (a!!0)in ((Claim id ((read (b!!0)), (read(b!!1))) dim), (a!!1))
+
         getDim ((Claim id pos _), str) = let a = splitOn "x" str in ((Claim id pos ((read $ a!!0), (read $ a!!1))), str)
 
         readClaim' = getDim . getPos . getId
