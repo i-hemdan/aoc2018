4b2b3cfc0bbf4e2ce6ccbf7dd0bf04d82b411886
Hemdan <ihemdan@QLI.local>
2018-12-04 09:42:49 -0700

    day2 part 1 complete

--- day 2/day2.hs
+++ day 2/day2.hs
@@ -8,7 +8,7 @@ import qualified Data.Map.Strict as Map
 main =  do  
         f <- readFile "input.txt"
         let ids = words f in
-            putStrLn $ show $ calcCheckSum $ splitTwosThrees $ useHasOfOnInput ids []
+            putStrLn $ show $ calcCheckSum $ useHasOfOnInput ids (0,0)
 
 
 hasOf :: String -> Map.Map Char Int -> [(Char, Int)]
@@ -16,27 +16,32 @@ hasOf string m = case string of
     [] -> Map.toList m
     (x:xs) -> let new_map = Map.insertWith (+) x 1 m in hasOf xs new_map
 
-useHasOfOnInput:: [String] -> [(Char, Int)]  -> [(Char, Int)]
+useHasOfOnInput:: [String] -> (Int, Int)  -> (Int, Int)
 useHasOfOnInput input out = case input of
     [] -> out
     (x:xs) -> let   tarr = hasOf x Map.empty 
-                    new_out = lsJoin tarr out
+                    twos_threes = splitTwosThrees tarr
                     in
-                        useHasOfOnInput xs new_out
+                        case twos_threes of
+                            ((x1:xs1), (x2:xs2))-> let new_out = addTup out (1, 1) in useHasOfOnInput xs new_out
+                            ([], (x1:xs1))      -> let new_out = addTup out (0, 1) in useHasOfOnInput xs new_out
+                            ((x1:xs1), [])      -> let new_out = addTup out (1, 0) in useHasOfOnInput xs new_out
+                            ([], [])            -> useHasOfOnInput xs out
 
 splitTwosThrees:: [(Char, Int)] -> ([(Char, Int)], [(Char, Int)])
 splitTwosThrees ls = let    ls1 = [el | el <- ls, let (_, x) = el in x == 2]
                             ls2 = [el2 | el2 <- ls, let (_, x2) = el2 in x2 == 3]
-                            in
-                                (ls1, ls2)
+                            in (ls1, ls2)
 
-calcCheckSum tuple_list = let   (a, b) = tuple_list
-                                lena = length a
-                                lenb = length b
-                                in
-                                    lena * lenb
+calcCheckSum tuple = let (a, b) = tuple in a * b
 
 lsJoin:: [a] -> [a] -> [a]
 lsJoin a b = case a of
     [] -> b
     (x:xs) -> lsJoin xs (x:b)      
+
+addTup a b = 
+    let (w, x) = a
+        (y, z) = b
+        in ((w + y), (x + z))
+
