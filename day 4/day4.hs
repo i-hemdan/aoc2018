85b76f25cd31157510abb1ea59474155a233eb40
Hemdan <ihemdan@QLI.local>
2018-12-05 16:18:02 -0700

    stuff

--- day 4/day4.hs
+++ day 4/day4.hs
@@ -23,9 +23,16 @@ data    Action =    BeginShift  |
 --      Action   
 
 data    TimeStamp = TimeStamp  
-                    { ts_year   ::Int
-                    , ts_month  ::Int
-                    , ts_day    ::Int
-                    , ts_hour   ::Int
-                    , ts_minute ::Int }
+                    { ts_year   ::Integer
+                    , ts_month  ::Integer
+                    , ts_day    ::Integer
+                    , ts_hour   ::Integer
+                    , ts_minute ::Integer }
 --      TimeStamp
+instance Eq TimeStamp where
+    (TimeStamp y m d h mi ) == (TimeStamp y2 m2 d2 h2 mi2) = 
+        fin1 == fin2
+        where
+            fin1 = (y*12*30*24*60)+(m*30*24*60)+(d*24*60)+(mi)
+            fin2 = (y2*12*30*24*60)+(m2*30*24*60)+(d2*24*60)+(mi2)
+--      Eq TimeStamp
