7b491dac714759e7380e5a181405b6b33da7fff0
Hemdan <ihemdan@QLI.local>
2018-12-05 15:27:47 -0700

    starting day 4

---
+++ day 4/day4.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Data.List.Split
+import qualified Data.Map.Strict as Map
+
+
+
+
+
+main = putStrLn "tst"
+
+
+
+
+
+data    TimeStamp = TimeStamp  
+                    { ts_year   ::Int
+                    , ts_month  ::Int
+                    , ts_day    ::Int
+                    , ts_hour   ::Int
+                    , ts_minute ::Int }
+--      TimeStamp
