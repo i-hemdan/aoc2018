9b36290effd7e6b0b630c5b502e9671473091453
i hemdan <>
2018-12-05 23:10:30 -0700

    timestamp parser

--- day 4/day4.hs
+++ day 4/day4.hs
@@ -7,7 +7,7 @@ import qualified Data.Map.Strict as Map
 
 
 
-main = putStrLn $ show $ (TimeStamp 2018 12 4 12 05) > (TimeStamp 2018 12 5 12 05)
+main = putStrLn $ show $ parseEvent "[2018-12-05 10:24]"
 
 
 
@@ -15,6 +15,22 @@ data    Event = Event
                 { e_time    ::TimeStamp
                 , e_guard   ::String
                 , e_action  ::Action }
+
+parseEvent str =
+    let
+        parseTimeStamp s1 =
+            let (bdate:timeb:rest) = splitOn " " s1
+                (_:date) = bdate
+                time = take 5 timeb 
+                (y:m:d:_) = splitOn "-" date
+                (h:mi:_) = splitOn ":" time
+                (yn, mn, dn, hn, min) = (read y, read m, read d, read h, read mi)
+                ts = (TimeStamp yn mn dn hn min)
+                in ts
+        in
+            parseTimeStamp str
+                
+
 --      Event
 
 data    Action =    BeginShift  |
@@ -27,7 +43,8 @@ data    TimeStamp = TimeStamp
                     , ts_month  ::Integer
                     , ts_day    ::Integer
                     , ts_hour   ::Integer
-                    , ts_minute ::Integer }
+                    , ts_minute ::Integer 
+                    } deriving (Show)
 
 toMinutes (TimeStamp y m d h mi) = (y*12*30*24*60)+(m*30*24*60)+(d*24*60)+(mi)
 
