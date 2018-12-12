301573b2747f4288985b8919d0f8d5c2367a5154
Hemdan <ihemdan@QLI.local>
2018-12-12 14:39:46 -0700

    P

--- day 6/day6.hs
+++ day 6/day6.hs
@@ -2,7 +2,9 @@ module Main where
 
 import Data.List.Split
 
+data Point a = P a a deriving(Show)
+
 main = readFile "input.txt" >>= (\f -> putStrLn $ show $ parse f)
 
 
-parse str = [let (a:b:[])=(splitOn ", " s) in (read a,read b)::(Int, Int)| s<-(splitOn "\n" str)]
+parse str = [let (a:b:[])=(splitOn ", " s) in P (read a::Int) (read b::Int)| s<-(splitOn "\n" str)]
