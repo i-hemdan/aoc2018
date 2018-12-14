4fbe7d100974e83a4d3ca59ce8e98673251966eb
Hemdan <ihemdan@QLI.local>
2018-12-14 12:36:13 -0700

    easier to read list comp

--- day 6/day6.hs
+++ day 6/day6.hs
@@ -8,5 +8,5 @@ data Point a = P a a deriving(Show)
 
 main = readFile "input.txt" >>= (\f -> putStrLn $ show $ parse f)
 
-parse str = [let (a:b:[])=(splitOn ", " s) in P (read a::Int) (read b::Int)| s<-(splitOn "\n" str)]
+parse str = [P (read a::Int) (read b::Int)|s<-(splitOn "\n" str), let (a:b:[])=(splitOn ", " s)]
 manD (P x1 y1) (P x2 y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
