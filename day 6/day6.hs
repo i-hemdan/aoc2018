bf774f59041c198d2172252a1e0ceda4ed013f2d
Hemdan <ihemdan@QLI.local>
2018-12-14 16:25:02 -0700

    instance stuff

--- day 6/day6.hs
+++ day 6/day6.hs
@@ -1,12 +1,17 @@
 module Main where
 
+import Data.List
 import Data.List.Split
 
 
 
-data Point a = P a a deriving(Show)
+data Point = P Int Int deriving(Show)
+instance Eq Point where
+    (P x y) == (P x1 y1) = (x == x1) && (y == y1)
+instance Ord Point where
+    compare (P x y) (P x1 y1)|y < y1 = LT|y == y1 && x < x1 = LT|y == y1 && x == x1 = EQ|otherwise = GT
 
-main = readFile "input.txt" >>= (\f -> putStrLn $ show $ parse f)
+main = readFile "input.txt" >>= (\f -> putStrLn $ show $ sort $ parse f)
 
 parse str = [P (read a::Int) (read b::Int)|s<-(splitOn "\n" str), let (a:b:[])=(splitOn ", " s)]
 manD (P x1 y1) (P x2 y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
