850e0f8c8e81a4540e871e9e686210f94f2d2a4c
Hemdan <ihemdan@QLI.local>
2018-12-14 12:30:02 -0700

    manD

--- day 6/day6.hs
+++ day 6/day6.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Data.List.Split
 
+
+
 data Point a = P a a deriving(Show)
 
 main = readFile "input.txt" >>= (\f -> putStrLn $ show $ parse f)
 
-
 parse str = [let (a:b:[])=(splitOn ", " s) in P (read a::Int) (read b::Int)| s<-(splitOn "\n" str)]
+manD (P x1 y1) (P x2 y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
