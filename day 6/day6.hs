17728a433ce94c118164657b2de58fd35013d0c1
i-hemdan <i_hemdan@hotmail.com>
2018-12-14 19:14:03 -0700

    more

--- day 6/day6.hs
+++ day 6/day6.hs
@@ -3,26 +3,18 @@ module Main where
 import Data.List
 import Data.List.Split
 
+main = readFile "input.txt" >>= (\f -> putStrLn $ show $ genPlane $ sort $ parsePoints f)
 
+calcAllDists ls ls2 = [(P x y [(b, manD a b) | b<-ls2]) | a@(P x y _)<-ls]
 
-
-
-main = readFile "input.txt" >>= (\f -> putStrLn $ show $ (!!0) $ reverse $ genPlane $ sort $ parsePoints f)
-
-genPlane ls = 
-    let 
-        ((P fx fy _):_) = ls
-        ((P lx ly _):_) = reverse ls
-        (xmod, ymod) = (abs (fx-lx), abs (fy-ly))
-        begin = (P (fx-xmod) (fy-ymod) [])
-        end = (P (lx+xmod) (ly+ymod) [])
-        in genFromTo begin end
-        
+genPlane ls = let   ((P fx fy _):_) = ls
+                    ((P lx ly _):_) = reverse ls
+                    (xmod, ymod) = (abs (fx-lx), abs (fy-ly))
+                    (begin, end) = ((P (fx-xmod) (fy-ymod) []), (P (lx+xmod) (ly+ymod) [])) in genFromTo begin end
 
 parsePoints str = [P (read a::Int) (read b::Int) []|s<-(splitOn "\n" str), let (a:b:[])=(splitOn ", " s)]
 
 manD (P x1 y1 _) (P x2 y2 _) = (abs $ x1 - x2) + (abs $ y1 - y2)
-
 
 data Point = P Int Int [(Point, Int)] deriving(Show)
 instance Eq Point where (P x y _) == (P x1 y1 _) = (x == x1) && (y == y1)
